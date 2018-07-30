12070ac17f376fca64aa6d84432c5c7080305fc1
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-07-30 12:05:25 -0500

    Installed Bulma and Bloomer; got a title page working

---
+++ src/LoginView/Login.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react"
+import {Title} from "bloomer"
+
+export default class Login extends Component {
+  render() {
+    return(
+      <Title>Vocabulous</Title>
+    )
+  }
+}
