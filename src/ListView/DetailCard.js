5f318f89f219346465f1821b1b6548e3b422dc24
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-08-01 14:59:31 -0500

    It works! Clicking multiple words switches details using componentDidUpdate hook

--- src/ListView/DetailCard.js
+++ src/ListView/DetailCard.js
@@ -5,8 +5,11 @@ import { Card, Title } from 'bloomer';
 export default class DetailCard extends Component {
 
   state = {
+    // wordId: this.props.wordId,
     wordToRender: {}
   }
+
+// The components above this are changing state, but it's not affecting props of this component, so it won't update
 
   componentDidMount() {
     console.log("mount", this.props.wordId)
@@ -15,6 +18,18 @@ export default class DetailCard extends Component {
           console.log(word)
           this.setState({wordToRender: word[0]});
         })
+  }
+
+  componentDidUpdate(oldProps) {
+    if(this.props.wordId !== oldProps.wordId) {
+
+      console.log("update", this.props.wordId)
+      LocalApi.getWordById(this.props.wordId)
+      .then(word => {
+        console.log(word)
+        this.setState({wordToRender: word[0]});
+      })
+    }
   }
 
   render() {
