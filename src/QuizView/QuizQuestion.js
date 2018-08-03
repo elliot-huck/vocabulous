78988bb91ed0d7e26157703bdc5bc00b79c554f0
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-08-03 16:02:14 -0500

    got answers to change color on a click

--- src/QuizView/QuizQuestion.js
+++ src/QuizView/QuizQuestion.js
@@ -6,11 +6,19 @@ export default class QuizQuestion extends Component {
 
 
   checkAnswer = (evt) => {
-    console.log(evt.target.textContent)
+    const userSelection = evt.target.textContent;
+    if (userSelection === this.props.currentQuestion.rightAnswer) {
+      console.log("Correct!")
+      evt.target.classList += " is-success"
+    } else {
+      console.log("Try again...")
+      evt.target.classList += " is-danger"
+    }
+
   }
 
   render() {
-    // console.log(this.props.questionList[this.props.currentQuestion].otherAnswers)
+
     const currentQuestion = this.props.currentQuestion;
     const answers = currentQuestion.otherAnswers;
 
@@ -20,25 +28,35 @@ export default class QuizQuestion extends Component {
         <Tile isAncestor>
 
           <Tile isParent isVertical>
+
             <Tile isChild>
               <Tag onClick={(evt) => { this.checkAnswer(evt) }}>
                 {answers[0]}
               </Tag>
             </Tile>
+
             <Tile isChild>
               <Tag onClick={(evt) => { this.checkAnswer(evt) }}>
                 {answers[1]}
               </Tag>
             </Tile>
+
           </Tile>
 
           <Tile isParent isVertical>
+
             <Tile isChild>
-              <Tag onClick={(evt) => { this.checkAnswer(evt) }}>{answers[2]}</Tag>
+              <Tag onClick={(evt) => { this.checkAnswer(evt) }}>
+                {answers[2]}
+              </Tag>
             </Tile>
+
             <Tile isChild>
-              <Tag onClick={(evt) => { this.checkAnswer(evt) }}>{answers[3]}</Tag>
+              <Tag onClick={(evt) => { this.checkAnswer(evt) }}>
+                {answers[3]}
+              </Tag>
             </Tile>
+
           </Tile>
 
         </Tile>
