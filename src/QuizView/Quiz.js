42bfb0b780b05326a10a333c6bf8103c8fabacfc
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-08-03 10:14:38 -0500

    Got a basic quiz view working

--- src/QuizView/Quiz.js
+++ src/QuizView/Quiz.js
@@ -1,10 +1,37 @@
 // This module renders the main Quiz view, where the user can take quizzes of words in their word list
 
 import React, { Component } from 'react';
+import { Columns } from "bloomer"
+import QuizStats from "./QuizStats"
 
 export default class Quiz extends Component {
 
+  state = {
+    takingQuiz: false
+  }
+
+  startQuiz = () => {
+    this.setState({ takingQuiz: true });
+  }
+
+  finishQuiz = () => {
+    this.setState({ takingQuiz: false })
+  }
+
   render() {
-    return <main>Quiz Page</main>
+    if (this.state.takingQuiz) {
+      return (
+        <Columns isCentered isVCentered>
+          <h1>Taking a quiz now</h1>
+        </Columns>
+      )
+    } else {
+      return (
+        <Columns isCentered isVCentered>
+          <QuizStats begin={() => {this.startQuiz()}} />
+        </Columns>
+      )
+    }
+
   }
 }
---
+++ src/QuizView/QuizStats.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Title, Column, Card } from "bloomer"
+import { Button } from '../../node_modules/bloomer/lib/elements/Button';
+
+export default class QuizStats extends Component {
+
+
+  render() {
+    return(
+      <Card>
+      <Title>You've taken [some] quizzes! Would you like to start a new one?</Title>
+      <Button onClick={() => {this.props.begin()}}>Start quiz!</Button>
+      </Card>
+    )
+  }
+}
