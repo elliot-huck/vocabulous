ac0efaab9b72048a11384d5598cb1d781a0a1382
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-08-06 16:01:31 -0500

    Set up ExternalApi; calls currently working

--- src/BrowseView/Browse.js
+++ src/BrowseView/Browse.js
@@ -1,10 +1,15 @@
 // This module renders the main Browse view, where the user can browse words to add to their list
 
 import React, { Component } from 'react';
+import ExternalApi from "../Api/ExternalApi"
 
 export default class Browse extends Component {
 
   render() {
+    ExternalApi.getMoreWords()
+      .then(response => {
+        console.log(response)
+      })
     return <main>Browse Page</main>
   }
 }
