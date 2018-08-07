aef476cc74f7ad06ac9d0985f9db4c40a740295b
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-08-07 13:33:10 -0500

    Renders button only when the detail view is showing; still need to add function to save word in database

--- src/BrowseView/BrowseCard.js
+++ src/BrowseView/BrowseCard.js
@@ -12,23 +12,41 @@ export default class BrowseCard extends Component {
     this.setState({ showingDetails: true })
   }
 
+  clicked = () => {
+    console.log("clicked")
+  }
+
+  // componentDidUpdate(oldProps) {
+  //   if (this.props !== oldProps) {
+  //     this.setState({ showingDetails: true })
+  //   }
+  // }
+
   render() {
-    // if (this.state.showingDetails) {
-    //   return (
-    //     <React.Fragment>
-    //       <BrowseDetails wordToRender={this.props.wordObject} />
-    //     </React.Fragment>
-    //   )
-    // }
-    return (
-      <Card>
-        <Title className={`${this.props.targetNumber}`} onClick={this.props.showDetails}>{this.props.show.word}</Title>
-        <p>{this.props.show.partOfSpeech}</p>
-        <p>{this.props.show.definition}</p>
-        <p>{this.props.show.sentence}</p>
-      <Button isColor="success">Add word</Button>
-        {/* <Button isColor="danger">Not interested</Button> */}
-      </Card>
-    )
+
+    if (this.state.showingDetails) {
+
+      return (
+        <Card>
+          <Title className={`${this.props.targetNumber}`} onClick={this.props.showDetails}>{this.props.show.word}</Title>
+          <p>{this.props.show.partOfSpeech}</p>
+          <p>{this.props.show.definition}</p>
+          <p>{this.props.show.sentence}</p>
+          <Button onClick={console.log("add clicked")}>Add to list</Button>
+          {/* <Button isColor="danger">Not interested</Button> */}
+        </Card>
+      )
+    } else {
+      return (
+        <Card>
+          <Title className={`${this.props.targetNumber}`} onClick={this.props.showDetails}>{this.props.show.word}</Title>
+          <p>{this.props.show.partOfSpeech}</p>
+          <p>{this.props.show.definition}</p>
+          <p>{this.props.show.sentence}</p>
+          {this.props.button}
+          {/* <Button isColor="danger">Not interested</Button> */}
+        </Card>
+      )
+    }
   }
 }
--- src/BrowseView/BrowsePane.js
+++ src/BrowseView/BrowsePane.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Box, Title } from 'bloomer';
 import BrowseCard from './BrowseCard';
+import { Button } from '../../node_modules/bloomer/lib/elements/Button';
 
 export default class BrowsePane extends Component {
 
@@ -11,12 +12,21 @@ export default class BrowsePane extends Component {
         <span>Click on a word to see its definition and add it to your list</span>
         {this.props.wordBatch.map(eachWord => {
           const targetNumber = this.props.wordBatch.indexOf(eachWord)
-          return <BrowseCard
-            key={targetNumber}
-            targetNumber={targetNumber}
-            showDetails={this.props.showDetails}
-            show={eachWord} />
+          let addButton = <span></span>
+          if (eachWord.definition) {
+            addButton = <Button>Add to list</Button>
+          }
+          return (
+            <BrowseCard
+              key={targetNumber}
+              targetNumber={targetNumber}
+              showDetails={this.props.showDetails}
+              show={eachWord}
+              button={addButton} />
+          )
+
         })}
+
       </Box>
     )
   }
