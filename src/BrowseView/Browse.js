b04b68f1cfd4a683a9c11757ecadb4b1f35967e2
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-08-07 09:39:11 -0500

    Created the browse pane

--- src/BrowseView/Browse.js
+++ src/BrowseView/Browse.js
@@ -2,14 +2,62 @@
 
 import React, { Component } from 'react';
 import ExternalApi from "../Api/ExternalApi"
+import BrowsePane from "./BrowsePane"
 
 export default class Browse extends Component {
 
+
+  state = {
+    currentWordBatch: [{
+      word: "test",
+      partOfSpeech: "noun",
+      definition: "trying something to find out about it",
+      sentence: "This is only a test.",
+      wordFrequency: 4.9,
+      id: 1
+    },
+    {
+      word: "example",
+      partOfSpeech: "noun",
+      definition: "a representative form or pattern",
+      sentence: "This is an example of a sentence.",
+      wordFrequency: 4.67,
+      id: 2
+    },
+    {
+      word: "word",
+      partOfSpeech: "noun",
+      definition: "a group of letters that conveys meaning",
+      sentence: "This sentence has five words.",
+      wordFrequency: 4.9,
+      id: 3
+    },
+    {
+      word: "owl",
+      partOfSpeech: "noun",
+      definition: "the best animal",
+      sentence: "Owls are basically the cats of the bird world.",
+      wordFrequency: 4.67,
+      id: 4
+    }]
+  }
+
+  newWordBatch = () => {
+
+  }
+
+
   render() {
-    ExternalApi.getMoreWords()
-      .then(response => {
-        console.log(response)
-      })
-    return <main>Browse Page</main>
+
+
+    // ExternalApi.getMoreWords()
+    //   .then(response => {
+    //     console.log(response)
+    //   })
+    // [{id: word: ""}]
+
+    return (
+      <BrowsePane />
+    )
   }
 }
---
+++ src/BrowseView/BrowsePane.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { Box } from '../../node_modules/bloomer/lib/elements/Box';
+
+export default class BrowsePane extends Component {
+
+  render() {
+    return (
+      <Box>This is the browse pane</Box>
+    )
+  }
+}
