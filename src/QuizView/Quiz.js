c53f1a7849b99636248b4debd2c769fb618bade7
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-08-08 16:06:28 -0500

    Added box container to make quiz look better on mobile

--- src/QuizView/Quiz.js
+++ src/QuizView/Quiz.js
@@ -1,7 +1,7 @@
 // This module renders the main Quiz view, where the user can see how many quizzes they've completed and take quizzes of words in their word list
 
 import React, { Component } from 'react';
-import { Columns } from "bloomer"
+import { Columns, Box } from "bloomer"
 import QuizStats from "./QuizStats"
 import QuizPane from "./QuizPane"
 
@@ -25,14 +25,16 @@ export default class Quiz extends Component {
     if (this.state.quizInProgress) {
       return (
         <Columns isCentered isVCentered>
-          <QuizPane end={() => {this.finishQuiz()}} />
+          <QuizPane end={() => { this.finishQuiz() }} />
         </Columns>
       )
     } else {
       return (
-        <Columns isCentered isVCentered>
-          <QuizStats begin={() => {this.startQuiz()}} />
-        </Columns>
+        <Box>
+          <Columns isCentered isVCentered>
+            <QuizStats begin={() => { this.startQuiz() }} />
+          </Columns>
+        </Box>
       )
     }
 
